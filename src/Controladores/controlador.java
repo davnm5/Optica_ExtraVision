ec28c95679cdff6d9ee8342606a222664f54c377
davnm5 <josdanei@espol.edu.ec>
2017-12-10 01:37:01 -0500

    Primer avance, se realiza la creación de los scenes que tendrá el sistema

---
+++ src/Controladores/controlador.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author creditos
+ */
+public class controlador implements Initializable {
+    @FXML private TextField usuario;
+    @FXML private PasswordField password;
+    @FXML private AnchorPane inicio,submenu,registros,clientes,proveedores;
+
+      
+    
+    
+   
+    @FXML
+    private void ingresar(ActionEvent event) throws IOException {
+        
+        if(usuario.getText().equals("")&&password.getText().equals("")){
+               
+        AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/submenu.fxml"));
+        this.inicio.getChildren().setAll(pane);
+    
+            
+            System.out.println("Ingreso Correcto");
+        }
+        else{
+            System.out.println("Ingreso Incorrecto");
+        }
+        }
+        
+    
+    @FXML
+    private void b_proveedores(ActionEvent event) throws IOException {
+        AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/proveedores.fxml"));
+        this.registros.getChildren().setAll(pane);
+    }
+    
+    
+    @FXML
+    private void b_clientes(ActionEvent event) throws IOException {
+        AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/clientes.fxml"));
+        this.registros.getChildren().setAll(pane);
+    }
+    
+    
+    @FXML
+    private void registrar(ActionEvent event) throws IOException {
+        AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/registros.fxml"));
+        this.submenu.getChildren().setAll(pane);
+    }
+    
+    
+    @FXML
+     private void regresar(ActionEvent event) throws IOException {
+      AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/principal.fxml"));
+      this.submenu.getChildren().setAll(pane);
+     }
+ 
+     @FXML
+     private void atras(ActionEvent event) throws IOException {
+      AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/submenu.fxml"));
+      this.registros.getChildren().setAll(pane);
+     }
+     
+     @FXML
+     private void atras_Clientes(ActionEvent event) throws IOException {
+      AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/registros.fxml"));
+      this.clientes.getChildren().setAll(pane);
+     }
+     @FXML
+     private void atras_Proveedores(ActionEvent event) throws IOException {
+      AnchorPane pane= FXMLLoader.load(this.getClass().getResource("/Componentes/registros.fxml"));
+      this.proveedores.getChildren().setAll(pane);
+     }
+     
+    @FXML
+     private void salir(ActionEvent event) {
+        System.exit(0);
+    }
+     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/optica_extravision/Optica_ExtraVision.java
@@ -0,0 +1,43 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package optica_extravision;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author creditos
+ */
+public class Optica_ExtraVision extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("OPTICA EXTRA VISION");
+        stage.getIcons().add(new Image("Media/icono.png"));
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/Componentes/principal.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
